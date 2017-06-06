e2575d4324ca346bf2f0a7cea275aae2f607cf2e
lcarlossandberg <carlossandberg@googlemail.com>
2017-06-06 13:29:55 +0100

    updated

---
+++ exp_layout_gen.m
@@ -0,0 +1,12 @@
+run_main = []//This is where the different experiment runs go
+
+//This is where initial conditions go
+
+main runnumber =
+
+//This is where the experiment goes
+
+where
+{
+    //This is where the definition of each recurrence relations go
+}
