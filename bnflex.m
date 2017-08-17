1886bc454a5de72bfd21c6b0a9b1694005541a02
lcarlossandberg <carlossandberg@googlemail.com>
2017-08-17 22:22:44 +0100

    new syntax

--- bnflex.m
+++ bnflex.m
@@ -1,26 +1,47 @@
-<``init''> ::= ``init''
-<[<letter>]> ::= ``var'' [<letter>]
-<[<letter>] ``_'' [letter]> ::= ``equation'' [<letter>] [<letter>]
-<``cond''> ::= ``cond''
-<``main''> ::= ``main''
-<``where''> ::= ``where''
-<``tl''> ::= ``tl''
-<``hd''> ::= ``hd''
-<``:''> ::= ``:''
-<``_'' [<letter>]> ::= ``intfunc'' [<letter>]
-<number> ::= number
-<``+''> ::= ``plus''
-<``-''> ::= ``minus''
-<``/''> ::= ``divide''
-<``*''> ::= ``multiply''
-<``!''> ::= ``bang''
-<``(''> ::= ``leftbracket''
-<``)''> ::= ``rightbracket''
-<``[''> ::= ``leftsquare''
-<``]''> ::= ``rightsuare''
-<``=''> ::= ``equal''
-<``~=''> ::= ``notequal''
-<``<''> ::= ``lessthan''
-<``>''> ::= ``greaterthan''
-<``<=''> ::= ``lessthanequalto''
-<``>=''> ::= ``greaterthanequalto''
+<simulation>  ::= <mainblock> <newline> <initblock> <newline> <whereblock>
+<mainblock>   ::= <main> <expressions>
+<initblock>   ::= <init> <expressions>
+<whereblock>  ::= <where> <recrels> | <where> <expressions>
+<expressions> ::= <expression> | <expression> <expressions>
+<recrels>     ::= <recrel> | <recrel> <recrels>
+<letters>     ::= <letter> | <letter> <letters>
+<number>      ::= <digit> | <digit> <number>
+<expression>  ::= <cond> <expression> <expression> <expression>
+                | <brackets>
+                | <list>
+                | <expression> <operation> <expression>
+                | <function>
+                | <variable>
+                | <internop> <expression>
+                | <number>
+                | <whereblock>
+<main>        ::= ``main\n''
+<init>        ::= ``init\n''
+<whereblock>  ::= ``where\n''
+<recrel>      ::= <letters> ``_'' <letters>
+<letter>      ::= `a' .. 'z' | `A' .. `Z'
+<cond>        ::= ``cond ''
+<brackets>    ::= ``('' <expression> ``)''
+<list>        ::= ``['' <expression>``]''
+                | ``['' <expression> ``,'' <list>
+                | <expression> ``,''
+                | <expression> ``]''
+<operation>   ::= ``+''
+                | ``-''
+                |``+''
+                |``-''
+                |``*''
+                |``/''
+                |``<''
+                |``>''
+                |``=''
+                |``~=''
+                |``<=''
+                |``>=''
+                |``:''
+                |``!''
+<function>    ::= `_' <letters> | `_' <letters> <expressions> | `_' <letters> <expressions> `=' <expression>
+<variable>    ::= <letters> | <letters> `=' <expression>
+<internop>    ::= ``hd'' | ``tl''
+<digit>       ::= `0' .. `9'
+<newline>     ::= `\n'
