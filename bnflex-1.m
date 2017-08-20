0a55f55c5eeb435964ef60fb730695cb72239965
lcarlossandberg <carlossandberg@googlemail.com>
2017-08-20 15:11:05 +0100

    nearly there

--- bnflex-1.m
+++ bnflex-1.m
@@ -1,5 +1,5 @@
 <simulation>           ::= <mainblock> <newline> <initblock> <newline> <whereblock>
-<mainblock>            ::= <main> <newline> <recrelname> <space> <numbers>
+<mainblock>            ::= <main> <newline> <recrelname> <space> <args>
 <numbers>              ::= <number> | <number> <space> <numbers>
 <initblock>            ::= <init>    | <init> <newline> <valdefinitions>
 <whereblock>           ::= <where> | <where> <newline> <recreldefinitions>
